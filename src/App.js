afe5853b4329afc74304a1820cdb317f9af10ba0
stefanstefanovic94 <stefan.stefanovic@kortechs.io>
2021-06-11 15:31:20 +0200

    log in css is done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react"
 import Login from "./components/login/Login"
 import { Switch, Link, Route } from "react-router-dom"
 import Wallet from "./components/wallet/Wallet"
+import "./app.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Switch>
         <Route exact path="/" component={Login}></Route>
         <Route exact path="/wallet" component={Wallet}></Route>
